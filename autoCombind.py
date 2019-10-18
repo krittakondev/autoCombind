1037ee44e4f7520110e9aab7431b05ee51b7e28e
krit <9krittakon@gmail.com>
2019-10-18 12:33:55 +0700

    update

---
+++ autoCombind.py
@@ -0,0 +1,135 @@
+import tkinter
+import pdfrw
+import tkinter.filedialog
+import tkinter.messagebox
+import os
+from tkinter import ttk
+import datetime
+
+
+class GUI():
+    def __init__(self, title):
+        self.root = tkinter.Tk()
+        self.root.title(title)
+        #self.root.geometry("300x300")
+        
+    def add_frame(self):
+        return tkinter.Frame(self.root)
+
+        
+    def fileSelect(self):
+        return tkinter.filedialog.askopenfilenames()
+    def mainloop(self):
+        self.root.mainloop()
+
+
+class run_program(GUI):
+    def add_pageInfo(self, pageDict, msg):
+        pageDict.krit = msg
+        return pageDict
+    
+    def is_odd(self, file):
+        page_info = pdfrw.PdfFileReader(file).pages
+        if len(page_info) % 2 == 0:
+            return False
+        else:
+            return True
+    def combind_loop(self, files, file_index, fileHeader=[], outfile="ไฟล์รวม"):
+        blankPage = "blank.pdf"
+        writer = pdfrw.PdfWriter()
+        cur_dir = os.path.dirname(files[0])
+        if len(fileHeader) > 0:
+            for head in fileHeader:
+                writer.addpages(pdfrw.PdfFileReader(head).pages)
+                if self.is_odd(head):
+                    writer.addpage(pdfrw.PdfFileReader(blankPage).pages[0])
+           
+        for i in range(len(files)):
+            if file_index != "":
+                add_index = self.add_pageInfo(pdfrw.PdfFileReader(file_index).pages[i], "index_insert")
+                writer.addpage(add_index)
+                writer.addpage(pdfrw.PdfFileReader(blankPage).pages[0])
+            if self.is_odd(files[i]):
+                writer.addpages(pdfrw.PdfFileReader(files[i]).pages)
+                writer.addpage(pdfrw.PdfFileReader(blankPage).pages[0])
+            elif len(files)-1 == i:
+                writer.addpages(pdfrw.PdfFileReader(files[i]).pages)
+                writer.addpage(pdfrw.PdfFileReader(blankPage).pages[0])
+                writer.addpage(pdfrw.PdfFileReader(blankPage).pages[0])
+            else:
+                writer.addpages(pdfrw.PdfFileReader(files[i]).pages)
+        saveTo = cur_dir+"/"+outfile+".pdf"
+        writer.write(saveTo)  
+        writer.write("log/"+datetime.datetime.now().strftime("%y%m%d_%S%M%H.pdf"))
+        return saveTo
+    def onclick_seFile(self):
+        files = self.fileSelect()
+        self.list_filename = files
+        for i in range(len(files)):
+            self.listFile.insert(tkinter.END, files[i])
+        self.insert_index.config(values=self.listFile.get(0, tkinter.END))
+    def clear_alllist(self):
+        self.listFile.delete(0, tkinter.END)
+        self.insert_index.config(values=self.listFile.get(0, tkinter.END))
+        self.insert_index.set("เลือกไฟล์หน้าคั่นบท")
+        self.fileHeader = []
+        self.showHead.config(text="")
+    def get_listFile(self):
+        return self.listFile.curselection()
+    def set_fileHeader(self):
+        cur_se = self.listFile.curselection()
+        fileHeader = self.listFile.get(cur_se)
+        self.fileHeader.append(fileHeader)
+        self.showHead.config(text=self.showHead.cget("text")+"\n"+fileHeader)
+        self.listFile.delete(cur_se)
+    
+    def onclick_combind(self):
+        try:
+            index_selected = self.listFile.get(0, "end").index(self.insert_index.get())
+            self.listFile.delete(index_selected)
+            saveTo = self.combind_loop(self.listFile.get(0, tkinter.END), self.insert_index.get(), self.fileHeader, self.fileout.get())
+            tkinter.messagebox.showinfo("รวมไฟล์","รวมไฟล์สำเรียบร้อยไฟล์จะเก็บไว้ที่ "+saveTo)
+        except Exception as e:
+            if str(e) == "tuple.index(x): x not in tuple":
+
+                        
+                saveTo = self.combind_loop(self.listFile.get(0, tkinter.END), "", self.fileHeader, self.fileout.get())
+                tkinter.messagebox.showinfo("รวมไฟล์","รวมไฟล์สำเรียบร้อยไฟล์จะเก็บไว้ที่ "+saveTo)
+                
+    def main_gui(self):
+        self.frame_left = self.add_frame()
+        self.frame_right = self.add_frame()
+        self.frame_menu = self.add_frame()
+        self.frame_left.grid(row=1, column=1)
+        self.frame_right.grid(row=1, column=2)
+        self.frame_menu.grid(row=2, column=2)
+        self.listFile = tkinter.Listbox(self.frame_left, width=50)
+        self.insert_index = ttk.Combobox(self.frame_right, values=self.listFile.get(0, tkinter.END), width=40)
+        self.insert_index.set("เลือกไฟล์หน้าคั่นบท")
+        tkinter.Label(self.frame_left, text="ไฟล์แต่ละบท", font=("Courier", 30)).pack()
+        self.listFile.pack(side=tkinter.LEFT)
+        tkinter.Button(self.frame_right, text="เลือกสาระบัญและปกใน", font=("Courier", 15), command=lambda: self.set_fileHeader()).pack()
+        self.showHead = tkinter.Label(self.frame_right, text="", font=("Courier", 10))
+        self.showHead.pack()
+        self.insert_index.pack(side=tkinter.LEFT)       
+        tkinter.Button(self.frame_menu, text="เลือกไฟล์", font=("Courier", 13), command=lambda: self.onclick_seFile()).pack()
+        tkinter.Button(self.frame_menu, text="clear", command=lambda: self.clear_alllist()).pack()
+        defaultName = tkinter.StringVar()
+        defaultName.set("รวมไฟล์")
+        self.fileout = tkinter.Entry(self.frame_menu, font=("Courier", 13), textvariable=defaultName)
+        self.fileout.pack()
+        tkinter.Button(self.frame_menu, text="combind", fg="pink", bg="green", command=lambda: self.onclick_combind()).pack()
+        self.root.mainloop()
+    
+    #def pageInsert(self):
+        
+    
+
+if __name__ == "__main__":
+    #root = tkinter.Tk()
+    #root.withdraw()
+    #file = tkinter.filedialog.askopenfilenames()
+    #combind_loop(file)
+    run = run_program("autoCombind")
+    run.fileHeader = []
+    run.main_gui()
